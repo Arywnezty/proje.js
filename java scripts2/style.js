a4843ab2bcacec3484beee0e7bbdf8ccf6d7231c
Arywnezty <ezzatiaryan13@gmail.com>
2025-02-22 11:11:42 +0330

    second commit

---
+++ java scripts2/style.js
@@ -0,0 +1,27 @@
+/* برای اولین مربع*/
+const box = document.getElementById("box");
+const red = Math.floor(Math.random() * 256);
+const green = Math.floor(Math.random() * 256);
+const blue = Math.floor(Math.random() * 256);
+box.style.backgroundColor = `rgb(${red} ,${green} ,${blue})`;
+
+/* برای دومین مربع*/
+const box1 = document.getElementById("box-1");
+const red1 = Math.floor(Math.random() * 256);
+const green1 = Math.floor(Math.random() * 256);
+const blue1 = Math.floor(Math.random() * 256);
+box1.style.backgroundColor = `rgb(${red1} ,${green1} ,${blue1})`;
+
+/* برای سومین مربع*/
+const box2 = document.getElementById("box-2");
+const red2 = Math.floor(Math.random() * 256);
+const green2 = Math.floor(Math.random() * 256);
+const blue2 = Math.floor(Math.random() * 256);
+box2.style.backgroundColor = `rgb(${red2} ,${green2} ,${blue2})`;
+
+/* برای چهارمین مربع*/
+const box3 = document.getElementById("box-3");
+const red3 = Math.floor(Math.random() * 256);
+const green3 = Math.floor(Math.random() * 256);
+const blue3 = Math.floor(Math.random() * 256);
+box3.style.backgroundColor = `rgb(${red3} ,${green3} ,${blue3})`;
