b0810083d85ab07b2dbecdd3cbedf511f2c9ce48
Arywnezty <ezzatiaryan13@gmail.com>
2025-03-01 13:22:57 +0330

    five commit

---
+++ java scripts5/style.js
@@ -0,0 +1,10 @@
+const images=document.getElementById("image");
+const rights=document.getElementById("right");
+const lefts=document.getElementById("left");
+
+function left(){
+images.setAttribute("src","./image/css.webp" ,"./image/js.jpg")
+}
+function right(){
+    images.setAttribute("src","./image/js.jpg")
+}
